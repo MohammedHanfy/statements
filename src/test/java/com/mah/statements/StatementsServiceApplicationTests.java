d24dadfc20a91cd628159d9850c233a7f9f002f8
MohammedHanfy <mohammed.ahmed.hanfy@gmail.com>
2022-02-12 20:05:43 +0400

    initial commit

---
+++ src/test/java/com/mah/statements/StatementsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mah.statements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatementsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
